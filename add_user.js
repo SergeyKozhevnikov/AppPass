0b863cb8d67dff0af7a0e674bd7e6fae090b4703
Kupited <voytsekhovskiy.va89@yandex.ru>
2025-03-14 01:27:11 +0300

    Added files add_user.(html, css, js), anonim.jpg, anonim2.png

---
+++ add_user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MenuContainer from './MenuContainer';
+
+ReactDOM.render(
+    <div>
+        <MenuContainer/>
+    </div>,
+    document.getElementById('container')
+);
