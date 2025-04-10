e2406f29bd14e2141d299f5e81897e62facbc29a
AEGabrielyan <aegabrielyan@greenatom.ru>
2025-04-10 23:40:56 +0500

    Feat: набросок - боковое меню

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,17 @@
-// Обертка для других страниц
-import './Layout.css';
-import { Header } from './Header/Header';
-import { Sidebar } from './Sidebar/Sidebar';
+import "./Layout.css";
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const showSidebar: boolean = true;
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen h-screen">
       <Header />
-      <div className="layout__wrapper">
+      <div className="layout__wrapper flex flex-1 min-h-screen h-full">
         {showSidebar && <Sidebar />}
-        <main className="layout__main">{children}</main>
+        <main className="layout__main flex-1 min-h-screen h-full">{children}</main>
       </div>
-    </>
+    </div>
   );
 }
