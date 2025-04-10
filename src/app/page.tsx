7fcfff00357f415f869167539da87e916f52f322
AEGabrielyan <aegabrielyan@greenatom.ru>
2025-04-10 23:46:33 +0500

    Fix: small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="sidebar border-2 rounded m-0.5 px-5 py-5 min-w-[340px] min-h-screen h-full" style={{ borderColor: "#007EC0" }}>
+    <div className="sidebar m-0.5 min-w-[340px] min-h-screen h-full">
       <main className="">Главная страница</main>
     </div>
   );
