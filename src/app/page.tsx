fd6e40e0b46d370c345d1c64c6365a90e0003783
Darpeex <foxyyysp@mail.ru>
2025-04-12 11:19:23 +0300

    refactor: layout с шапкой и меню вынесен в отдельную подгруппу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 // Главная страница - MainPage (Андрей Б)
+import { Layout } from '@/components/layout/Layout';
 
 export default function Home() {
   return (
-    <div className="sidebar m-0.5 min-w-[340px] min-h-screen h-full">
-      <main className="">Главная страница</main>
-    </div>
+    <Layout>
+      <div className="sidebar m-0.5 min-w-[340px] min-h-screen h-full">
+        <main className="">Главная страница</main>
+      </div>
+    </Layout>
   );
 }
