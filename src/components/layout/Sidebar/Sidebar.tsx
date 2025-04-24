3d93351fc35f156eac2aa6fefa1064480a63562f
Darpeex <foxyyysp@mail.ru>
2025-04-24 23:07:24 +0300

    refactor: "О системе" - теперь модальное окно

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -2,18 +2,21 @@
 
 import Link from "next/link";
 import {
-    List,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Divider,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Divider,
 } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { ConfigurableSidebar } from "@/components/ConfigurableSidebar"; // Подключаем новый компонент
 import { signOut } from "next-auth/react";
+import { useState } from "react";
+import AboutPage from "@/components/AboutModal";
 
 export function Sidebar() {
+    const [isModalAboutSystemOpen, setIsModalAboutSystemOpen] = useState(false);
     const headerHeight = process.env.NEXT_PUBLIC_HEADER_HEIGHT;
 
     return (
@@ -23,9 +26,9 @@ export function Sidebar() {
                 padding: "1rem",
                 borderColor: "#007EC0",
                 height: `calc(100vh - ${headerHeight}px)`,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
             }}
             className="px-5 py-5 layout__main flex-1 h-full border border-2 m-0.5 rounded"
         >
@@ -34,15 +37,24 @@ export function Sidebar() {
 
             <Divider sx={{ my: 2 }} />
 
-
-            {/* Второй список прижат к низу */}
-            <List style={{ marginTop: 'auto' }}>
-                <ListItemButton component={Link} href="/about">
-                    <ListItemIcon>
+              {/* Второй список прижат к низу */}
+              <List style={{ marginTop: "auto" }}>
+                  <ListItemButton
+                      component={Link}
+                      href="#"
+                      onClick={() => setIsModalAboutSystemOpen(true)}
+                  >
+                      <ListItemIcon>
                         <InfoIcon />
                     </ListItemIcon>
                     <ListItemText primary="О системе" />
                 </ListItemButton>
+                {isModalAboutSystemOpen && (
+                    <AboutPage
+                        isOpen={isModalAboutSystemOpen}
+                        setIsOpen={setIsModalAboutSystemOpen}
+                    />
+                )}
 
                 <ListItemButton
                     component={Link}
