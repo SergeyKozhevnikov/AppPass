956a9bd2686a587f1d9d001dadfab34f4abfcfb0
AEGabrielyan <aegabrielyan@greenatom.ru>
2025-04-25 16:05:06 +0500

    Вернуть ссылку на dashboard

--- src/components/menus/DashboardMenu.tsx
+++ src/components/menus/DashboardMenu.tsx
@@ -2,9 +2,14 @@ import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import LockIcon from "@mui/icons-material/Lock";
 import GroupIcon from "@mui/icons-material/Group";
 import Link from "next/link";
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 
 export const DashboardMenu = () => (
   <>
+    <ListItemButton component={Link} href="/dashboard">
+      <ListItemIcon><TextSnippetIcon /></ListItemIcon>
+      <ListItemText primary="Дашборд" />
+    </ListItemButton>
     <ListItemButton component={Link} href="/passwords">
       <ListItemIcon>
         <LockIcon />
