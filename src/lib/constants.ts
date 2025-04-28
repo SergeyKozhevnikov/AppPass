2282c69ff95325b6d7db7a72b7566841098fb006
Darpeex <foxyyysp@mail.ru>
2025-04-28 14:33:40 +0300

    refactor: точки перехода экрана из MUI

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,6 @@
 // Константы, для удобного изменения их в одном файле
 
-// Например, точки перестроения экрана
-export const LAPTOP = '(max-width: 1024px)';
-export const TABLET = '(max-width: 768px)';
-export const MOBILE = '(max-width: 550px)';
+// MUI точки перестроения экрана - https://mui.com/material-ui/customization/breakpoints/
 
 // Константа для авторизации пользователя
 export const AUTH_FIELDS = {
