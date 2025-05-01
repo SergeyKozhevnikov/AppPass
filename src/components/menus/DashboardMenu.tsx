8e571728b4e666d73730d21a86c104250c576a55
Darpeex <foxyyysp@mail.ru>
2025-05-01 14:25:07 +0300

    refactor: удалена кнопка нового пользователя из меню

--- src/components/menus/DashboardMenu.tsx
+++ src/components/menus/DashboardMenu.tsx
@@ -1,5 +1,4 @@
 import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import LockIcon from "@mui/icons-material/Lock";
 import GroupIcon from "@mui/icons-material/Group";
 import Link from "next/link";
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
@@ -9,12 +8,6 @@ export const DashboardMenu = () => (
     <ListItemButton component={Link} href="/dashboard">
       <ListItemIcon><TextSnippetIcon /></ListItemIcon>
       <ListItemText primary="Дашборд" />
-    </ListItemButton>
-    <ListItemButton component={Link} href="/passwords">
-      <ListItemIcon>
-        <LockIcon />
-      </ListItemIcon>
-      <ListItemText primary="Новый пользователь" />
     </ListItemButton>
     <ListItemButton component={Link} href="/roles">
       <ListItemIcon>
