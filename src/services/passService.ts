a26b99f8a0557b164a8431bbfa573ba1c8de8534
AEGabrielyan <aegabrielyan@greenatom.ru>
2025-05-11 16:03:48 +0500

    Feat: добавить выборку из реальной базы

---
+++ src/services/passService.ts
@@ -0,0 +1,19 @@
+export type Pass = {
+  id: number;
+  date: string;
+  lastName: string;
+  firstName: string;
+  middleName: string;
+  hasCar: boolean;
+  hasMaterials: boolean;
+  status: 'drafts' | 'inReview' | 'approved' | 'rejected'; // Добавлено свойство status
+};
+
+export const fetchPasses = async (): Promise<Pass[]> => {
+  const response = await fetch('http://localhost:3001/api/passes');
+  if (!response.ok) {
+    throw new Error('Ошибка при получении заявок');
+  }
+  const data = await response.json();
+  return data.data; // Возвращаем только массив из поля data
+};
