1e5604f89e50faba347a975a6d9d07a1338a2554
Darpeex <foxyyysp@mail.ru>
2025-05-30 20:53:01 +0300

    feat: боковое меню определяется ролью пользователя

--- src/components/ConfigurableSidebar.tsx
+++ src/components/ConfigurableSidebar.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { usePathname } from 'next/navigation'; // Хук для получения текущего пути
-import { List } from "@mui/material";
-import { DashboardMenu } from "@/components/menus/DashboardMenu";
-import { DefaultMenu } from "@/components/menus/DefaultMenu";
+import { List } from '@mui/material';
+import { DashboardMenu } from '@/components/menus/DashboardMenu';
+import { UserMenu } from '@/components/menus/UserMenu';
 
 export function ConfigurableSidebar() {
   const pathname = usePathname(); // Получаем текущий путь
@@ -11,14 +11,14 @@ export function ConfigurableSidebar() {
   // Логика для изменения списка в зависимости от пути
   const renderMenuItems = () => {
     switch (true) {
-      case pathname.includes("/dashboard"):
+      case pathname.includes('/dashboard'):
         return <DashboardMenu />;
-      case pathname.includes("/profile"):
+      case pathname.includes('/profile'):
         return <DashboardMenu />;
-      case pathname.includes("/roles"):
+      case pathname.includes('/roles'):
         return <DashboardMenu />;
       default:
-        return <DefaultMenu />;
+        return <UserMenu />;
     }
   };
 
